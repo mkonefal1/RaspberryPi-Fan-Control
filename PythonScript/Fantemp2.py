13eea2b141da14639db6fa612db6c7691c8007c8
Mateusz <mkonefal1@gmail.com>
2023-09-13 21:57:45 +0200

    clear code

--- PythonScript/Fantemp2.py
+++ PythonScript/Fantemp2.py
@@ -14,8 +14,6 @@ GPIO.setwarnings(False)
 
 GPIO.setup(pin, GPIO.OUT)
 
-# I've changed the code to allow the fan to work on a determine temperature range.
-# This is to avoid continuous turning the fan on and off when it is around the original desired temperature. I set the temp_limit to 50.
 if cputemp < temp_limit:
     GPIO.output(pin, GPIO.LOW)
 
